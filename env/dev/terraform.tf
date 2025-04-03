fb3746c4dcb1534b9a38696b49914cbf1f8fa8ef
Hakim Afarmach <lthat@MacBook-Air-de-Hakim.local>
2025-04-04 01:16:00 +0200

    init state

---
+++ env/dev/terraform.tf
@@ -0,0 +1,23 @@
+# Description: Terraform configuration file for the dev environment
+# Author: Lthat
+
+terraform {
+  required_version = ">= 1.8.5"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=4.25.0"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name   = "rg-openrmscore-dev"  
+    storage_account_name  = "openrmscoredevsa01"    
+    container_name        = "tfstate-dev"             
+    key                   = "terraform.tfstate"   
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
