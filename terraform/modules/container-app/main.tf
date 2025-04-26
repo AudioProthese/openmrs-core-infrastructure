d3b0c594e1a8bca646e3c5a3d746f788ebeefe27
Lthath <115538665+Lthath@users.noreply.github.com>
2025-04-26 22:43:15 +0200

    fix: switch to consumption plan due to workload profile quota restrictions (#32)

--- terraform/modules/container-app/main.tf
+++ terraform/modules/container-app/main.tf
@@ -7,11 +7,14 @@ resource "azurerm_container_app_environment" "this" {
   logs_destination               = var.logs_destination
   log_analytics_workspace_id     = var.log_analytics_workspace_id
 
-  workload_profile {
-    name                  = var.workload_profile_name
-    workload_profile_type = var.workload_profile_type
-    minimum_count         = var.workload_profile_min_count
-    maximum_count         = var.workload_profile_max_count
+  dynamic "workload_profile" {
+    for_each = var.workload_profile_name != "" ? [1] : []
+    content {
+      name                  = var.workload_profile_name
+      workload_profile_type = var.workload_profile_type
+      minimum_count         = var.workload_profile_min_count
+      maximum_count         = var.workload_profile_max_count
+    }
   }
 
   tags = var.tags
@@ -22,7 +25,7 @@ resource "azurerm_container_app" "app" {
   resource_group_name          = var.resource_group_name
   container_app_environment_id = azurerm_container_app_environment.this.id
   revision_mode                = var.revision_mode
-  workload_profile_name        = var.workload_profile_name
+  workload_profile_name        = var.workload_profile_name != "" ? var.workload_profile_name : null
 
   template {
     min_replicas = var.min_replicas
