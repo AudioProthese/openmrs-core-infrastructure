de3c29d523e211e64f731feda70aa52d3db1c1a3
Toxma <matteo.zinutti@gmail.com>
2025-05-12 23:30:20 +0200

    fix: remove prevent_destroy lifecycle rule from Azure Container Registry and Key Vault resources

--- terraform/dev/acr.tf
+++ terraform/dev/acr.tf
@@ -7,7 +7,4 @@ resource "azurerm_container_registry" "acr" {
   resource_group_name = azurerm_resource_group.rg.name
   location            = var.location
   sku                 = "Standard"
-  lifecycle {
-    prevent_destroy = true
-  }
 }
