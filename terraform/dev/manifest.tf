d36bcd196c657c83b9178292a5f81b11ec5da45b
Toxma <matteo.zinutti@gmail.com>
2025-05-13 11:03:25 +0200

    feat: add namespace and service account for Loki Helm chart

--- terraform/dev/manifest.tf
+++ terraform/dev/manifest.tf
@@ -1,5 +1,16 @@
+# Create the namespace
+resource "kubectl_manifest" "namespace_loki" {
+  yaml_body = <<YAML
+apiVersion: v1
+kind: Namespace
+metadata:
+  name: loki
+YAML
+}
+
+# Create the ServiceAccount
 resource "kubectl_manifest" "sa_loki" {
-  depends_on = [helm_release.loki]
+  depends_on = [kubectl_manifest.namespace_loki]
   yaml_body  = <<YAML
 apiVersion: v1
 kind: ServiceAccount
