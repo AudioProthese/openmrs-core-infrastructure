3d195c03558a611748a0b4812e128b0c381cc9a4
Toxma <matteo.zinutti@gmail.com>
2025-05-13 09:23:37 +0200

    fix: update data source reference for Azure Container Registry in role assignment

--- terraform/dev/data.tf
+++ terraform/dev/data.tf
@@ -4,3 +4,8 @@
 
 data "azurerm_client_config" "current" {
 }
+
+data "azurerm_container_registry" "acr" {
+  name                = "openmrscoredevacr"
+  resource_group_name = var.resource_group
+}
