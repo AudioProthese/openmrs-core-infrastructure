f13c9ac02824b7ca1eb58b6106444f53586d44b0
Toxma <matteo.zinutti@gmail.com>
2025-05-15 11:22:16 +0200

    fix: update Terraform version to 1.11.4 in configuration files

--- terraform/dev/providers.tf
+++ terraform/dev/providers.tf
@@ -3,7 +3,7 @@
 ##########################
 
 terraform {
-  required_version = "= 1.11.3"
+  required_version = "= 1.11.4"
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
