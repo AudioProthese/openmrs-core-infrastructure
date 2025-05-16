53445b19fd868661216cab210d1712c746251797
Toxma <79527885+Toxma@users.noreply.github.com>
2025-05-16 22:48:05 +0200

    feat(ingress): Add OpenMRS Ingress resource with TLS configuration (#98)

--- terraform/dev/manifests.tf
+++ terraform/dev/manifests.tf
@@ -24,4 +24,40 @@ spec:
 YAML
 }
 
+##########################
+# OpenMRS Ingress
+##########################
+
+resource "kubectl_manifest" "openmrs_ingress" {
+  depends_on = [helm_release.openmrs]
+  yaml_body  = <<YAML
+apiVersion: networking.k8s.io/v1
+kind: Ingress
+metadata:
+  annotations:
+    cert-manager.io/cluster-issuer: letsencrypt-prod
+    nginx.ingress.kubernetes.io/force-ssl-redirect: "true"
+  name: openmrs-gateway-ingress
+  namespace: openmrs
+spec:
+  ingressClassName: webapprouting.kubernetes.azure.com
+  rules:
+    - host: openmrs.dev.audioprothese.ovh
+      http:
+        paths:
+          - path: /
+            pathType: Prefix
+            backend:
+              service:
+                name: gateway
+                port:
+                  number: 80
+  tls:
+    - secretName: openmrs-tls
+      hosts:
+        - openmrs.dev.audioprothese.ovh
+YAML
+}
+
+
 
