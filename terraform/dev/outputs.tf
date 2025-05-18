fe1b2f8f628556b63992428eae42f418a0090931
Toxma <matteo.zinutti@gmail.com>
2025-05-18 19:08:01 +0200

    refactor(terraform): Remove OVH name servers configuration and outputs

--- terraform/dev/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "name_servers" {
-  value = azurerm_dns_zone.audioprothese_ovh.name_servers
-}
--- terraform/dev/providers.tf
+++ terraform/dev/providers.tf
@@ -17,10 +17,6 @@ terraform {
       source  = "gavinbunney/kubectl"
       version = "1.19.0"
     }
-    ovh = {
-      source  = "ovh/ovh"
-      version = "2.2.0"
-    }
   }
 
   backend "azurerm" {
@@ -55,8 +51,4 @@ provider "kubectl" {
   client_certificate     = base64decode(azurerm_kubernetes_cluster.aks.kube_config.0.client_certificate)
   client_key             = base64decode(azurerm_kubernetes_cluster.aks.kube_config.0.client_key)
   load_config_file       = false
-}
-
-provider "ovh" {
-  endpoint = "ovh-eu"
 }
