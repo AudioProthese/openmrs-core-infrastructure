40804c07f269cf5fecb70f78d3eecaeddcfac139
Toxma <matteo.zinutti@gmail.com>
2025-05-20 15:12:47 +0200

    feat(terraform): Uncomment ACR resource block and remove unused data source for Azure Container Registry

--- terraform/dev/acr.tf
+++ terraform/dev/acr.tf
@@ -2,9 +2,9 @@
 # Resource Group
 ##########################
 
-# resource "azurerm_container_registry" "acr" {
-#   name                = "${var.project}${var.env}acr"
-#   resource_group_name = var.resource_group
-#   location            = var.location
-#   sku                 = "Standard"
-# }
+resource "azurerm_container_registry" "acr" {
+  name                = "${var.project}acr${var.env}"
+  resource_group_name = var.resource_group
+  location            = var.location
+  sku                 = "Standard"
+}
