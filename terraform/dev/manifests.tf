1a58fe23cbca9065f99ed8210b5138ce6fcfa536
Toxma <79527885+Toxma@users.noreply.github.com>
2025-05-21 11:27:44 +0200

    Add External Secret Operator (#112)
    * feat(terraform): Add Cert Manager ClusterIssuer and ESO resources for Azure integration
    
    * feat: Add ESO Helm Chart and update related resources for Azure integration

--- terraform/dev/manifests.tf
+++ terraform/dev/manifests.tf
@@ -1,5 +1,5 @@
 #############################
-# Cert Manager Cluster Issuer
+# Cert Manager ClusterIssuer
 #############################
 
 resource "kubectl_manifest" "cluster_issuer" {
@@ -62,3 +62,77 @@ spec:
         - openmrs.dev.audioprothese.ovh
 YAML
 }
+
+#############################
+# ESO Service Account
+#############################
+
+resource "kubectl_manifest" "sa" {
+  depends_on = [helm_release.eso]
+  yaml_body  = <<YAML
+apiVersion: v1
+kind: ServiceAccount
+metadata:
+  annotations:
+    azure.workload.identity/client-id: "${azurerm_kubernetes_cluster.aks.kubelet_identity[0].client_id}"
+  name: workload-identity-sa
+  namespace: eso
+YAML
+}
+
+##############################
+# ESO ClusterSecretStore
+##############################
+
+resource "kubectl_manifest" "clustersecretstore" {
+  depends_on = [kubectl_manifest.sa]
+  yaml_body  = <<YAML
+apiVersion: external-secrets.io/v1
+kind: ClusterSecretStoreecretStore
+metadata:
+  name: azure-cluster-secret-store
+  annotations:
+    external-secrets.io/disable-maintenance-checks: "true"
+spec:
+  provider:
+    azurekv:
+      authType: WorkloadIdentity
+      vaultUrl: ${azurerm_key_vault.vault.vault_uri}
+      tenantId: ${azurerm_key_vault.vault.tenant_id}
+      serviceAccountRef:
+        name: workload-identity-sa
+  conditions:
+    - namespaces:
+        - "monitoring"
+        - "authgate"
+YAML
+}
+
+# ###############################
+# # ESO ExternalSecret
+# ###############################
+
+# resource "kubectl_manifest" "grafana_azuread_secret" {
+#   depends_on = [kubectl_manifest.secretstore]
+#   yaml_body  = <<YAML
+# apiVersion: external-secrets.io/v1beta1
+# kind: ExternalSecret
+# metadata:
+#   name: grafana-azuread-secret
+# spec:
+#   refreshPolicy: Periodic
+#   refreshInterval: 1h 
+#   secretStoreRef:
+#     name: azure-secret-store
+#     kind: SecretStore
+#   target:
+#     name: grafana-azuread-secret
+#   data:
+#   - secretKey: GF_AUTH_GENERIC_OAUTH_CLIENT_ID
+#     remoteRef:
+#       key: GF_AUTH_GENERIC_OAUTH_CLIENT_ID
+#   - secretKey: GF_AUTH_GENERIC_OAUTH_CLIENT_SECRET
+#     remoteRef:
+#       key: GF_AUTH_GENERIC_OAUTH_CLIENT_SECRET
+# YAML
+# }
